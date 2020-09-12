8d744eb06713bd250244870d2855b880888a1f3c
Alex Kim <45559664+Alex-K1m@users.noreply.github.com>
2020-09-12 16:04:24 +0500

    Add react boilerplate react project

---
+++ react/README.md
@@ -0,0 +1,14 @@
+# Gatsby starter with linting
+
+A Gatsby v2 starter with a single hello world component and some code quality checking tools.
+
+## Features
+
+- Only one page with a single dummy component
+- ESLint for `.js` & `.jsx`
+  - Airbnb config
+  - imports sorting
+  - prettier & jest integration
+- Prettier for formatting JSON, YAML, Markdown
+- Jest with react-test-renderer
+- Pre-commit linting, formatting & testing with husky & lint-staged
---
+++ react/gatsby-config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  siteMetadata: {},
+  plugins: [
+    { resolve: 'gatsby-plugin-react-helmet' },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: `${__dirname}/src/images`,
+      },
+    },
+    { resolve: 'gatsby-transformer-sharp' },
+    { resolve: 'gatsby-plugin-sharp' },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'gatsby-starter-linting',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'src/images/gatsby-icon.png',
+      },
+    },
+    // this optional plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // { resolve: 'gatsby-plugin-offline' },
+  ],
+};
