00968b9e7e74197a531e0214611e68ecf7e6fe1a
Alex Kim <45559664+Alex-K1m@users.noreply.github.com>
2020-09-25 19:56:55 +0500

    Add Chip component

--- react/src/fem/intermediate/job-listings-with-filtering/components/JobListings/JobListings.jsx
+++ react/src/fem/intermediate/job-listings-with-filtering/components/JobListings/JobListings.jsx
@@ -4,17 +4,14 @@ import 'typeface-spartan';
 import React from 'react';
 
 import Seo from '../../../../../components/Seo';
-import logo from '../../images/photosnap.svg';
-import Logo from '../Logo';
+import Chip from '../Chip';
 import styles from './JobListings.module.scss';
 
 const JobListings = () => (
   <>
     <Seo title='Job Listings' />
     <h1 className={styles.textCenter}>Hello from Job Listings</h1>
-    <div style={{ width: 100 }}>
-      <Logo src={logo} alt='Photosnap logo' />
-    </div>
+    <Chip color='dark'>Featured</Chip>
   </>
 );
 
