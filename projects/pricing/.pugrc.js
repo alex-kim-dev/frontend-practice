87394f4819aaa25ab3693a600a1dc922245ecf34
Alex Kim <45559664+Alex-K1m@users.noreply.github.com>
2020-11-19 14:50:10 +0500

    refactor(pricing): separate content from markup

--- projects/pricing/.pugrc.js
+++ projects/pricing/.pugrc.js
@@ -1,7 +1,9 @@
 const siteSettings = require('@frontend/site-meta');
+const content = require('./src/content.json');
 
 module.exports = {
   locals: {
     site: siteSettings,
+    content,
   },
 };
