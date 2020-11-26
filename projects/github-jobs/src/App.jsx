ae61615d9148890a25471ad09a400c4d96d26b42
Alex Kim <45559664+Alex-K1m@users.noreply.github.com>
2020-11-26 23:21:57 +0500

    style(github-jobs): add some global styling

--- projects/github-jobs/src/App.jsx
+++ projects/github-jobs/src/App.jsx
@@ -6,8 +6,38 @@ import reset from 'reset-jss';
 import Hello from './Hello';
 import theme from './theme';
 
+const globalStyles = {
+  '@global': {
+    '*, *::before, *::after': {
+      boxSizing: 'inherit',
+    },
+
+    html: {
+      boxSizing: 'border-box',
+      fontSize: '62.5%',
+    },
+
+    body: {
+      fontSize: '1.6rem',
+    },
+
+    '.sr-only': {
+      border: 0,
+      clip: 'rect(0 0 0 0)',
+      clipPath: 'inset(50%)',
+      height: 1,
+      overflow: 'hidden',
+      padding: 0,
+      position: 'absolute',
+      whiteSpace: 'nowrap',
+      width: 1,
+    },
+  },
+};
+
 const jss = createJss(preset());
 jss.createStyleSheet(reset).attach();
+jss.createStyleSheet(globalStyles).attach();
 
 const App = () => {
   return (
