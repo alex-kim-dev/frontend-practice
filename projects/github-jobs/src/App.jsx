366f083b68a61d477fd4b5006dabbb49a3ee2b0f
Alex Kim <45559664+Alex-K1m@users.noreply.github.com>
2020-11-26 16:21:16 +0500

    style(github-jobs): add reset styles

--- projects/github-jobs/src/App.jsx
+++ projects/github-jobs/src/App.jsx
@@ -1,13 +1,21 @@
-import { ThemeProvider } from 'react-jss';
+import { create as createJss } from 'jss';
+import preset from 'jss-preset-default';
+import { JssProvider, ThemeProvider } from 'react-jss';
+import reset from 'reset-jss';
 
 import Hello from './Hello';
 import theme from './theme';
 
+const jss = createJss(preset());
+jss.createStyleSheet(reset).attach();
+
 const App = () => {
   return (
-    <ThemeProvider theme={theme.light}>
-      <Hello />
-    </ThemeProvider>
+    <JssProvider jss={jss}>
+      <ThemeProvider theme={theme.light}>
+        <Hello />
+      </ThemeProvider>
+    </JssProvider>
   );
 };
 
