afc0ea3d31da83b076c1d2a3fda8fefcdf5cf1f3
Alex-K1m <45559664+Alex-K1m@users.noreply.github.com>
2021-09-11 07:44:20 +0000

    Deploying to gh-pages from @ Alex-K1m/frontend-practice@b0cf1c12d3bd24729fa7bb40a3cb6e82e6ac0941 🚀

---
+++ dashboard/index.9e3033fb.js
@@ -0,0 +1,2 @@
+document.querySelector('input[type="checkbox"]').addEventListener("click",(({target:e})=>{e.checked?document.body.setAttribute("theme","dark"):document.body.removeAttribute("theme")}));
+//# sourceMappingURL=index.9e3033fb.js.map
