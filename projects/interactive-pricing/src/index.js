6bac174eec5fdc0fd4e3dfbe8e049115d16522e0
Alex Kim <45559664+Alex-K1m@users.noreply.github.com>
2021-09-14 23:05:55 +0500

    chore(interactive-pricing): add challenge files

--- projects/interactive-pricing/src/index.js
+++ projects/interactive-pricing/src/index.js
@@ -1,6 +1,15 @@
 import './style.css';
 
-document.querySelector('.app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`;
+/*
+### Page view and pricing totals
+
+Here are the different page view ranges and the corresponding monthly price totals:
+
+- 10K pageviews / $8 per month
+- 50K pageviews / $12 per month
+- 100K pageviews / $16 per month
+- 500k pageviews / $24 per month
+- 1M pageviews / $36 per month
+
+If the visitor switches the toggle to yearly billing, a 25% discount should be applied to all prices.
+*/
