26af47ff3e097938db60bde4e6c6d114793db0ec
Alex Kim <45559664+alex-kim-dev@users.noreply.github.com>
2021-12-28 16:50:51 +0500

    feat(github-user): project setup

---
+++ projects/github-user/vite.config.js
@@ -0,0 +1,26 @@
+import siteMeta from '@frontend/site-meta';
+import { defineConfig } from 'vite';
+import { minifyHtml, injectHtml } from 'vite-plugin-html';
+
+export default defineConfig(({ mode }) => ({
+  base: `${
+    mode === 'production' ? siteMeta.baseurl.concat('/github-user') : ''
+  }/`,
+
+  root: 'src',
+
+  publicDir: '../static',
+
+  build: {
+    outDir: '../public',
+    emptyOutDir: true,
+    sourcemap: true,
+  },
+
+  plugins: [
+    minifyHtml(),
+    injectHtml({
+      data: siteMeta,
+    }),
+  ],
+}));
