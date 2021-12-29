7a10f3de15491e595f921b8709b486f1f2ac6f1a
Alex Kim <45559664+alex-kim-dev@users.noreply.github.com>
2021-12-29 11:21:42 +0500

    feat(github-user): add header

--- projects/github-user/src/index.js
+++ projects/github-user/src/index.js
@@ -1,1 +1,12 @@
 import './style.css';
+
+const $switch = document.querySelector('.theme-switch');
+
+let isDark = false;
+
+$switch.addEventListener('click', () => {
+  if (isDark) document.body.removeAttribute('data-theme');
+  else document.body.setAttribute('data-theme', 'dark');
+  $switch.setAttribute('aria-checked', !isDark);
+  isDark = !isDark;
+});
